4ec64b46f18089a9c1ceae66e3637f43eb789497
Chetana Parupalli <chetanaparupalli@Chetanas-MacBook-Pro-2.local>
2022-02-15 16:26:13 -0500

    fixed cart price calc

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -27,13 +27,17 @@ export class CartComponent implements OnInit {
   removeItem(item: Clothing): void{
     const index: number = this.cart.indexOf(item);
     this.cart.splice(index,1);
+    //console.log("new array: " + JSON.stringify(this.cart));
     this.calculatePrice();
   }
 
   calculatePrice(): void{
+    //console.log("Inside calc: " + JSON.stringify(this.cart));
+    this.totalPrice = 0;
     for(var item of this.cart){
       this.totalPrice += item.price;
     }
+    //console.log("Price: " + JSON.stringify(this.totalPrice));
   }
 
 }
